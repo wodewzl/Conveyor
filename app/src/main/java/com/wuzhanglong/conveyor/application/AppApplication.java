0a9237f072abbd29f9cf51fe58e1f4f18d254472
wuzhanglong <zhanglong.wu@lantaiyuan.com>
2018-01-09 13:26:16 +0800

    整体代码上传

--- app/src/main/java/com/wuzhanglong/conveyor/application/AppApplication.java
+++ app/src/main/java/com/wuzhanglong/conveyor/application/AppApplication.java
@@ -4,13 +4,12 @@ package com.wuzhanglong.conveyor.application;
 
 import android.app.Application;
 import android.content.Context;
-
+import android.support.multidex.MultiDex;
 
 import com.umeng.socialize.Config;
 import com.umeng.socialize.PlatformConfig;
 import com.umeng.socialize.UMShareAPI;
 import com.wuzhanglong.conveyor.model.UserInfoVO;
-import com.wuzhanglong.library.application.BaseAppApplication;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -19,13 +18,15 @@ import java.io.ObjectOutputStream;
 
 import cn.jpush.android.api.JPushInterface;
 
-public class AppApplication   extends android.support.multidex.MultiDexApplication  {
+public class AppApplication extends Application {
     private static AppApplication mAppApplication;
+
     {
-        PlatformConfig.setWeixin("wx93027a99f78841b5","12f61fc306634e3470d2b61de397296b");
-        PlatformConfig.setQQZone("1106607224","FWo7N3LPeqTdpkgV");
+        PlatformConfig.setWeixin("wx93027a99f78841b5", "12f61fc306634e3470d2b61de397296b");
+        PlatformConfig.setQQZone("1106607224", "FWo7N3LPeqTdpkgV");
 //        PlatformConfig.setSinaWeibo("319845988","fbb3df56985bcd557975e1bd8e8d8a1e","http://xiaojingsc.test.beisheng.wang/ht");
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -33,7 +34,7 @@ public class AppApplication   extends android.support.multidex.MultiDexApplicati
         JPushInterface.init(this);
         UMShareAPI.get(this);//初始化sdk
         //开启debug模式，方便定位错误，具体错误检查方式可以查看http://dev.umeng.com/social/android/quick-integration的报错必看，正式发布，请关闭该模式
-        Config.DEBUG = true;
+        Config.DEBUG = false;
 
     }
 
@@ -67,9 +68,9 @@ public class AppApplication   extends android.support.multidex.MultiDexApplicati
         }
     }
 
-//    @Override
-//    protected void attachBaseContext(Context base) {
-//        super.attachBaseContext(base);
-//        MultiDex.install(this);
-//    }
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 }
