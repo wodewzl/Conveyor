a21ae88c02693bf1dc58cdd630d33f0fdd68759b
wuzhanglong <273415345@qq.com>
2018-05-22 13:24:06 +0800

    ss

---
+++ app/src/main/java/com/wuzhanglong/conveyor/activity/PublishActivity.java
@@ -0,0 +1,123 @@
+package com.wuzhanglong.conveyor.activity;
+
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.cjt2325.cameralibrary.JCameraView;
+import com.cjt2325.cameralibrary.listener.ClickListener;
+import com.cjt2325.cameralibrary.listener.ErrorListener;
+import com.cjt2325.cameralibrary.listener.JCameraListener;
+import com.wuzhanglong.conveyor.R;
+import com.wuzhanglong.library.activity.BaseActivity;
+import com.wuzhanglong.library.mode.BaseVO;
+
+import java.io.File;
+
+public class PublishActivity extends BaseActivity {
+    JCameraView jCameraView;
+
+    @Override
+    public void baseSetContentView() {
+        contentInflateView(R.layout.publish_activity);
+    }
+
+    @Override
+    public void initView() {
+        jCameraView = (JCameraView) findViewById(R.id.jcameraview);
+
+//设置视频保存路径
+        jCameraView.setSaveVideoPath(Environment.getExternalStorageDirectory().getPath() + File.separator + "JCamera");
+
+//设置只能录像或只能拍照或两种都可以（默认两种都可以）
+        jCameraView.setFeatures(JCameraView.BUTTON_STATE_BOTH);
+
+//设置视频质量
+        jCameraView.setMediaQuality(JCameraView.MEDIA_QUALITY_MIDDLE);
+
+//JCameraView监听
+        jCameraView.setErrorLisenter(new ErrorListener() {
+            @Override
+            public void onError() {
+                //打开Camera失败回调
+                Log.i("CJT", "open camera error");
+            }
+
+            @Override
+            public void AudioPermissionError() {
+                //没有录取权限回调
+                Log.i("CJT", "AudioPermissionError");
+            }
+        });
+
+        jCameraView.setJCameraLisenter(new JCameraListener() {
+            @Override
+            public void captureSuccess(Bitmap bitmap) {
+                //获取图片bitmap
+                Log.i("JCameraView", "bitmap = " + bitmap.getWidth());
+            }
+
+            @Override
+            public void recordSuccess(String url, Bitmap firstFrame) {
+                //获取视频路径
+                Log.i("CJT", "url = " + url);
+            }
+            //@Override
+            //public void quit() {
+            //    (1.1.9+后用左边按钮的点击事件替换)
+            //}
+        });
+//左边按钮点击事件
+        jCameraView.setLeftClickListener(new ClickListener() {
+            @Override
+            public void onClick() {
+                PublishActivity.this.finish();
+            }
+        });
+//右边按钮点击事件
+        jCameraView.setRightClickListener(new ClickListener() {
+            @Override
+            public void onClick() {
+                Toast.makeText(PublishActivity.this, "Right", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    @Override
+    public void bindViewsListener() {
+
+    }
+
+    @Override
+    public void getData() {
+        showView();
+    }
+
+    @Override
+    public void hasData(BaseVO vo) {
+
+    }
+
+    @Override
+    public void noData(BaseVO vo) {
+
+    }
+
+    @Override
+    public void noNet() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        jCameraView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        jCameraView.onPause();
+    }
+}
